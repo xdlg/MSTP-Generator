53da9bb69ed8105566de383b4c3dccc701d71ec6
xdlg <xdell@live.fr>
2018-06-05 16:06:51 +0200

    Implement better colormap and add some timing code to the main (temporary)

--- include/colormap.h
+++ include/colormap.h
@@ -10,14 +10,29 @@
 #include <stdint.h>
 #include <math.h>
 
+/** Choice of colormap */
+typedef enum
+{
+    COLORMAP_BW,
+    COLORMAP_RAINBOW,
+    COLORMAP_LAVA
+} colormap_choice;
+
 /**************************************************************************//**
- * Color mapping for SDL pixel format RGB888.
+ * Generates the selected colormap.
+ * 
+ * @param[in] c Choosen colormap
+ *****************************************************************************/
+void colormap_init(colormap_choice c);
+
+/**************************************************************************//**
+ * Color mapping for SDL pixel format ARGB888.
  * 
  * @param[in] w Image width
  * @param[in] h Image height
  * @param[in] s Source image (pixels in the interval [0; 1])
- * @param[out] d Destination image (format RGB888)
+ * @param[out] d Destination image (format ARGB888)
  *****************************************************************************/
-void colormap_RGB888(uint32_t w, uint32_t h, float_t *s, uint32_t *d);
+void colormap_ARGB8888(uint32_t w, uint32_t h, float_t *s, uint32_t *d);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,15 @@
 #include <math.h>
 #include <stdlib.h>
 #include <SDL.h>
+#include <chrono>
 #include "muscatupa.h"
 #include "colormap.h"
 
 #define WIDTH       400 /**< Image width */
 #define HEIGHT      225 /**< Image height */
 #define N_SCALES    5   /**< Number of Turing patterns/scales */
+
+//~ #define TIMING_ACTIVE
 
 /** 
 * Parameters of the Turing patterns.
@@ -35,6 +38,12 @@ int main(int argc, char ** argv)
     uint32_t image_colormapped[WIDTH*HEIGHT];
     uint32_t i;
     
+#ifdef TIMING_ACTIVE
+    uint32_t n_frames = 1;
+    double_t t_average = 0.0;
+    double_t t_diff;
+#endif
+    
     SDL_Event event;
     bool quit = false;
     
@@ -47,7 +56,7 @@ int main(int argc, char ** argv)
 		p[i].wt = wt_all[i];
     }
     
-    // Randomize the image
+    colormap_init(COLORMAP_LAVA);
     muscatupa_init_image(WIDTH, HEIGHT, image);
     
     // Initialize SDL
@@ -56,7 +65,7 @@ int main(int argc, char ** argv)
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Texture * texture = SDL_CreateTexture(renderer,
-        SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+        SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
  
     while (!quit)
     {    
@@ -65,16 +74,30 @@ int main(int argc, char ** argv)
         {
             quit = event.type == SDL_QUIT;
         }
+ 
+#ifdef TIMING_ACTIVE
+        auto start = std::chrono::steady_clock::now();
+#endif
 
         // Update image
         muscatupa_step(p, N_SCALES, WIDTH, HEIGHT, image);
-        colormap_RGB888(WIDTH, HEIGHT, image, image_colormapped);
+        colormap_ARGB8888(WIDTH, HEIGHT, image, image_colormapped);
     
         // Show updated image
         SDL_UpdateTexture(texture, NULL, image_colormapped,
             WIDTH*sizeof(uint32_t));
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
+   
+#ifdef TIMING_ACTIVE
+        auto end = std::chrono::steady_clock::now();
+        t_diff = std::chrono::duration <double, std::milli> (end - start).count();
+        t_average = t_average + (t_diff - t_average)/n_frames;
+        n_frames++;
+        
+        if (n_frames == 200)
+            break;
+#endif
     }
     
     // Clean up SDL
@@ -82,6 +105,10 @@ int main(int argc, char ** argv)
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    
+#ifdef TIMING_ACTIVE
+    std::cout << t_average << std::endl;
+#endif
  
     return 0;
 }
