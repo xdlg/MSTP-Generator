94b938343081936afe2fc31e4f6788b3aca99362
xdlg <xdlg@pm.me>
2020-04-25 17:25:36 +0200

    Add libav wrapper and update CMakeLists accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
+/**
+ * @file
+ * @brief Main
+ */
+
+#include "VideoEncoder.hpp"
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "Hello world!" << std::endl;
+    int width = 800;
+    int height = 600;
+    int bitrate = 400000;
+    int framerate = 25;
+
+    VideoEncoder encoder("mpeg4", bitrate, width, height, framerate);
+
+    int ret = encoder.open("output.mpg");
+    if (ret < 0) {
+        std::cerr << "Cannot open encoder\n";
+        return -1;
+    }
+
+    uint8_t* rgbData = new uint8_t[3 * width * height];
+    for (int64_t j = 0; j < 3 * width * height; j += 3) {
+        rgbData[j] = 100;
+        rgbData[j + 1] = 0;
+        rgbData[j + 2] = 200;
+    }
+
+    for (int64_t i = 0; i < 25; i++) {
+        ret = encoder.encodeRgbData(rgbData, i);
+        if (ret < 0) {
+            std::cerr << "Cannot encode data\n";
+            encoder.close();
+            std::abort();
+        }
+    }
+
+    delete[] rgbData;
+
+    encoder.close();
 
     return 0;
 }
